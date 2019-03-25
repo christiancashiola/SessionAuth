99fe3833e80cec1d74a57f3f119a5381c6aded83
Christian Cashiola <cashiolachristian@gmail.com>
2019-03-25 12:50:16 -0700

    Add user model and joi validations

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -1,9 +1,21 @@
+import Joi from 'joi';
 import express from 'express';
+import User from '../models/user';
+import { signUp } from '../validations/user';
 
-const userRoutes = express.Router();
+const userRouter = express.Router();
+userRouter.post("", async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    await Joi.validate({ username, email, password }, signUp);
 
-userRoutes.post("", (req, res) => {
-  res.send(req.body);
+    const newUser = new User({ username, email, password });
+    await newUser.save();
+
+    res.send({ userId: newUser.id, username });
+  } catch (err) {
+    res.status(400).send(err);
+  }
 });
 
-export default userRoutes;
+export default userRouter;
