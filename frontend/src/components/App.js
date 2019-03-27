a6229a125b7d745db1fb2a5c7ecbfff0415a512c
Christian Cashiola <cashiolachristian@gmail.com>
2019-03-27 13:03:48 -0700

    Add components

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React from "react";
+import { Route } from "react-router-dom";
+import Welcome from "./Welcome";
+import Login from "./Login";
+import Signup from "./Signup";
+import Dashboard from "./Dashboard";
+import { AuthRoute, ProtectedRoute } from "../util/route";
 
 export default () => (
   <>
-   <h1>Hello world</h1>
+    <Route exact path="/" component={Welcome} />
+    <AuthRoute path="/login" component={Login} />
+    <AuthRoute path="/signup" component={Signup} />
+    <ProtectedRoute path="/dashboard" component={Dashboard} />
   </>
 );
