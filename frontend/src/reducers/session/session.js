ffe68fa570c9e2de52b7d80a69309126254e3745
Christian Cashiola <cashiolachristian@gmail.com>
2019-03-27 13:04:31 -0700

    Add _nullSession

--- frontend/src/reducers/session/session.js
+++ frontend/src/reducers/session/session.js
@@ -3,13 +3,14 @@ import {
   LOGOUT_CURRENT_USER
 } from "../../actions/session";
 
-export default (state = null, { type, user }) => {
+const _nullSession = { userId: null, username: null }
+export default (state = _nullSession, { type, user }) => {
   Object.freeze(state);
   switch (type) {
     case RECEIVE_CURRENT_USER:
       return user;
     case LOGOUT_CURRENT_USER:
-      return null;
+      return _nullSession;
     default:
       return state;
   }
